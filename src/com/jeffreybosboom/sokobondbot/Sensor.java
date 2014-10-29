d475305c34080394bffc737a7c2c2c4ab8030b1f
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-10-29 16:45:14 -0400

    Remove unused Sensor.gridSquares, organize color constants

--- src/com/jeffreybosboom/sokobondbot/Sensor.java
+++ src/com/jeffreybosboom/sokobondbot/Sensor.java
@@ -44,6 +44,13 @@ import javax.imageio.ImageIO;
  * @since 10/23/2014
  */
 public final class Sensor {
+	private static final int WHITE = 0xFFFFFFFF;
+	private static final int SQUARE_GRAY = 0xFFF0F0F0;
+	private static final int BLACK = 0xFF000000;
+	private static final Set<Integer> BOUNDARY_COLORS = Stream.of(
+			0xFFFFD452
+	).collect(toSet());
+
 	private final ImmutableList<Image> images;
 	private ImmutableSortedSet<Coordinate> boundary;
 	private ImmutableRangeSet<Integer> rowRanges, colRanges;
@@ -60,9 +67,6 @@ public final class Sensor {
 		return null;
 	}
 
-	private static final Set<Integer> BOUNDARY_COLORS = Stream.of(
-			0xFFFFD452
-	).collect(toSet());
 	private void determineBoundary() {
 		Set<Region> boundaries = Region.connectedComponents(images.get(0), BOUNDARY_COLORS).stream()
 				.filter(r -> r.points().size() > 25)
@@ -207,38 +211,6 @@ public final class Sensor {
 		return images.stream().map(i -> i.subimage(row, col, rows, cols));
 	}
 
-	private static final int SQUARE_GRAY = 0xFFF0F0F0;
-	private static final int BLACK = 0xFF000000;
-	private void gridSquares() {
-		//Gray if gray in any image, else black.
-		int[][] p = new int[images.get(0).rows()][images.get(0).cols()];
-		for (int r = 0; r < images.get(0).rows(); ++r)
-			for (int c = 0; c < images.get(0).cols(); ++c) {
-				int r_ = r, c_ = c;
-				p[r][c] = (images.stream().mapToInt(i -> i.at(r_, c_)).anyMatch(i -> i == SQUARE_GRAY)) ?
-						SQUARE_GRAY : BLACK;
-			}
-		Set<Region> squares = Region.connectedComponents(new Image(p), SQUARE_GRAY).stream()
-				.filter(r -> r.points().size() > 5)
-				.collect(toSet());
-
-		RangeSet<Integer> rowRanges = TreeRangeSet.create();
-		squares.stream().map(Region::rowSpan).forEachOrdered(rowRanges::add);
-		List<Range<Integer>> rows = rowRanges.asRanges().stream().collect(toList());
-		RangeSet<Integer> colRanges = TreeRangeSet.create();
-		squares.stream().map(Region::colSpan).forEachOrdered(colRanges::add);
-		List<Range<Integer>> cols = colRanges.asRanges().stream().collect(toList());
-
-		for (Region square : squares) {
-			Region.Point c = square.centroid();
-			Rectangle b = square.boundingBox();
-			int row = rows.indexOf(rowRanges.rangeContaining(c.row()));
-			int col = cols.indexOf(colRanges.rangeContaining(c.col()));
-			System.out.println(row +" "+col);
-
-		}
-	}
-
 	/**
 	 * Returns the element of the atom in the given square, or null if the
 	 * square is empty.
@@ -256,7 +228,6 @@ public final class Sensor {
 		return Element.fromColor(mostFrequentElementColor);
 	}
 
-	private static final int WHITE = 0xFFFFFFFF;
 	private static int recognizeElectrons(Image square) {
 		return Region.connectedComponents(square, WHITE).size();
 	}
